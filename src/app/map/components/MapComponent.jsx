eee570450ad6eca7ec27570f6ded1baec07f3804
Katia Krut <katiakrut488@gmail.com>
2024-05-03 21:45:06 +0300

    cleared e polygons

--- src/app/map/components/MapComponent.jsx
+++ src/app/map/components/MapComponent.jsx
@@ -16,31 +16,12 @@ function MapComponent() {
 
     const [cropPolygons, setCropPolygons] = useState([]);
     const [fPolygons, setfPolygons] = useState([]);
-    const [ePolygons, setePolygons] = useState([]);
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY, libraries,
     });
 
     if (loadError) return <div>Error loading maps</div>;
     if (!isLoaded) return <div>Loading Maps...</div>;
-
-    // const transformCoordinates = (coords) => {
-    //     if (!Array.isArray(coords)) {
-    //         console.error("Expected an array of coordinates, received:", coords);
-    //         return [];
-    //     }
-    //
-    //     return coords.map(polygon => {
-    //         // if (!Array.isArray(polygon) || polygon.some(coord => !Array.isArray(coord) || coord.length !== 2)) {
-    //         //     console.error("Invalid polygon data:", polygon);
-    //         //     return [];
-    //         // }
-    //
-    //         return polygon.map(([lng, lat]) => ({
-    //             lat: lat, lng: lng
-    //         }));
-    //     });
-    // };
 
     const transformCoordinates = (coords) => {
         if (!Array.isArray(coords)) {
@@ -49,26 +30,11 @@ function MapComponent() {
         }
 
         return coords.map(polygon => {
-            if (!Array.isArray(polygon)) {
-                console.error("Expected a polygon to be an array, received:", polygon);
-                return []; // Returning an empty array for this polygon as it's not in expected format
-            }
-
-            return polygon.map(coord => {
-                if (!Array.isArray(coord) || coord.length !== 2) {
-                    console.error("Expected coordinate to be an array of two elements, received:", coord);
-                    return null; // Returning null for individual coordinates that do not conform
-                }
-                const [lng, lat] = coord;
-                if (typeof lng !== 'number' || typeof lat !== 'number') {
-                    console.error("Coordinates must be numbers, received:", coord);
-                    return null;
-                }
-                return { lat, lng };
-            }).filter(coord => coord !== null); // Filter out any invalid coordinates
+            return polygon.map(([lng, lat]) => ({
+                lat: lat, lng: lng
+            }));
         });
     };
-
 
     const onPolygonComplete = polygon => {
         const coordinates = (polygon.getPath().getArray().map(coord => [coord.lng(), coord.lat()]));
@@ -83,14 +49,13 @@ function MapComponent() {
                 console.log(data);
                 polygon.setMap(null);
                 if (data.crop && Array.isArray(data.crop)) {
-                    setCropPolygons(transformCoordinates(data.crop));
+                    // console.log(transformCoordinates(data.crop))
+                    setCropPolygons(data.crop);
+                    // setCropPolygons(transformCoordinates(data.crop));
                 }
-                if (data.f && Array.isArray(data.f)) {
-                    setfPolygons(transformCoordinates(data.f));
-                }
-                if (data.e && Array.isArray(data.e)) {
-                    setePolygons(transformCoordinates(data.e));
-                }
+                // if (data.f && Array.isArray(data.f)) {
+                //     setfPolygons(transformCoordinates(data.f));
+                // }
             })
             .catch(error => console.error('Error:', error));
     };
@@ -117,8 +82,7 @@ function MapComponent() {
             }}
         />
         {renderPolygons(cropPolygons, 'green')}
-        {renderPolygons(fPolygons, 'blue')}
-        {renderPolygons(ePolygons, 'red')}
+        {/*{renderPolygons(fPolygons, 'blue')}*/}
     </GoogleMap>);
 
     function renderPolygons(polygons, color) {
