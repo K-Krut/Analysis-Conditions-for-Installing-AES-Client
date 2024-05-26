002b8bb3913ac7ae1c5ce22f9f9f3bbd9c304083
Katia Krut <katiakrut448@gmail.com>
2024-05-26 20:16:03 +0300

    wind info added to resources page

--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -1,10 +1,11 @@
 "use client"
 
+import Head from "next/head";
 import Footer from "@/app/components/Footer";
 import Navbar from "@/app/components/Navbar";
 import LandscapeTypeTable from "@/app/resources/components/LandTypesTable";
-import EnergyOutputFormula from "@/app/resources/components/EnergyOutputFormula";
-import Head from "next/head";
+import SolarEnergyOutputFormula from "@/app/resources/components/SolarEnergyOutputFormula";
+import WindEnergyOutputFormula from "@/app/resources/components/WindEnergyOutputFormula";
 
 export default function Resources() {
     return (
@@ -15,7 +16,8 @@ export default function Resources() {
         <main className="flex min-h-screen flex-col bg-[#121212]">
             <Navbar />
             <div className="container mt-24 mx-auto px-12 py-4 relative">
-                <EnergyOutputFormula />
+                <SolarEnergyOutputFormula />
+                <WindEnergyOutputFormula />
                 <LandscapeTypeTable />
             </div>
             <Footer />
