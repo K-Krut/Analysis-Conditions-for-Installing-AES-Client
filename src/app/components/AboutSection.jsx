e24a9c3903f6add4bad5e629e5861c09ccada481
Katia Krut <katiakrut448@gmail.com>
2024-05-26 20:23:37 +0300

    about section updated

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 
 const AboutSection = () => {
@@ -8,7 +9,7 @@ const AboutSection = () => {
                "padding-bottom": "150px"
              }}>
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/landsat2.jpg" width={500} height={500} />
+        <Image src="/images/landsat2.jpg" width={500} height={500}  alt={"landsat2.jpg"}/>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About</h2>
           <p className="text-base lg:text-lg">
